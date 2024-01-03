62988db5b36b8594243ee97c525abfb92fe78b36
Tobias Paxian <paxiant@informatik.uni-freiburg.de>
2024-01-03 15:51:43 +0100

    script can now minimize found errors

---
+++ script/callPacose.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+rm maxsat_proof.pbp
+echo "./callPacose.sh $1 $2"
+/usr/bin/timeout --kill-after=2s 2s ../bin/Pacose --TrimMaxSAT "$2"
+echo "grep -E \"$1|SHOULDNEVERHAPPEN\" maxsat_proof.pbp"
+grep -E "$1|SHOULDNEVERHAPPEN" maxsat_proof.pbp
+if [[ $? -eq "0" ]]; then
+  exit 1
+fi
+exit 0
--- script/fuzzTOTEST.sh
+++ script/fuzzTOTEST.sh
@@ -5,25 +5,36 @@ if [ -z "$1" ]; then
   ToTest="TOTEST"
 fi
 
+echo "Try to find $ToTest or SHOULDNEVERHAPPEN in the proof file!"
+
 wcnf="/tmp/$RANDOM$RANDOM$RANDOM.wcnf"
 
 (cd ../build && make -j)
-(cd wcnffuzz && make)
-#(cd wcnfddmin && make)
+(cd wcnffuzz && make -j)
+(cd wcnfdddmin && make -j)
 mv wcnffuzz/wcnfuzz .
-#mv wcnfddmin/wcnfddmin .
+mv wcnfdddmin/wcnfddmin .
 
-rv=1
+rv=0
 counter=0
-while [[ $rv -eq "1" ]]; do
+while [[ $rv -eq "0" ]]; do
     ((counter++))
+    echo ""
+    echo -E "ROUND $counter"
     ./wcnfuzz --wcnf > "$wcnf"
-    /usr/bin/timeout --kill-after=2s 2s ../bin/Pacose --TrimMaxSAT "$wcnf"
-    grep -E "$ToTest|SHOULDNEVERHAPPEN" maxsat_proof.pbp
+    ./callPacose.sh "$ToTest" "$wcnf"
     rv=$?
 done
 
-echo "Found $ToTest after $counter loops"
+echo "Grep found either $ToTest or SHOULDNEVERHAPPEN, reduce instance!"
+baseWcnf=$(basename "$wcnf")
+reducedWcnf="red-$baseWcnf"
+var="./wcnfddmin -e 0 -r $reducedWcnf -s ./callPacose.sh_$ToTest $wcnf"
+echo "$var"
+eval "$var"
+#./wcnfddmin -e 0 -r "$reducedWcnf" -s ./callPacose.sh_"$ToTest" "$wcnf"
+
+echo "Found $ToTest after $counter loops. It is reduced to $reducedWcnf"
+
 cp "$wcnf" .
-# TODO Tobias: Minimize wcnf
 rm "$wcnf"
