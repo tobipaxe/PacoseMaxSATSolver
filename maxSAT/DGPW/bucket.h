247a9d2c826726eed9a2aa2df73efb4ea8a31a2a
DietVds <vandesande.dieter@gmail.com>
2024-03-13 15:32:26 +0100

    fix derivation optimality for T=2^p-1 sat

--- maxSAT/DGPW/bucket.h
+++ maxSAT/DGPW/bucket.h
@@ -46,6 +46,7 @@ class Bucket {
   uint32_t kopt=UINT32_MAX; // index for the literal that is unsatisfiable in coarse convergence. 
 //   constraintid cxnCCsat=0; 
   constraintid cxnCCunsat=0;
+  constraintid cxnCCUB = 0;
 
   /**
    * @brief size of bucket.
